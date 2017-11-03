fb92b1a6cc178937ba7fee912615368ab6b1d264
Qiao Anran <qiaoanran@gmail.com>
2017-11-03 15:28:10 +0800

    add new button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import daoStyle from 'dao-style-package-vue';
 import $http from '@/utils/http';
+import userApi from '@/apis/user';
 
 import Noty from 'noty';
 import 'noty/lib/noty.css';
@@ -32,6 +33,21 @@ Vue.use(daoStyle);
 
 // axios
 Vue.prototype.$http = $http;
+
+router.beforeEach((to, from, next) => {
+  if (!localStorage.getItem('token')) {
+    window.location.href = '/login.html';
+  }
+
+  userApi.getCurrentUser().then((res) => {
+    if (res.name !== 'admin') {
+      window.location.href = '/login.html';
+    }
+    next();
+  }, () => {
+    window.location.href = '/login.html';
+  });
+});
 
 /* eslint-disable no-new */
 new Vue({
